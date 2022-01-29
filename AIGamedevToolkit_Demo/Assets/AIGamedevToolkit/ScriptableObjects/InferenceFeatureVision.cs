1726d6b6c2604abb883a789a77d8204878498db1
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-28 18:12:50 -0800

    Update InferenceFeatureVision.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/ScriptableObjects/InferenceFeatureVision.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/ScriptableObjects/InferenceFeatureVision.cs
@@ -4,63 +4,68 @@ using UnityEngine;
 using UnityEngine.Video;
 
 
-[CreateAssetMenu]
-[System.Serializable]
-public class InferenceFeatureVision : InferenceFeature
+namespace AIGamedevToolkit
 {
-    public InputTexture inputTexture;
-
-
-    [Tooltip("The target resolution for input images")]
-    public Vector2Int targetDims = new Vector2Int(640, 640);
-
-
-    // The unpadded dimensions of the image being fed to the model
-    public Vector2Int imageDims = new Vector2Int(0, 0);
-
-    // Used to scale the input image dimensions while maintaining aspect ratio
-    protected float aspectRatioScale;
-
-
-    public virtual void Inference(RenderTexture renderTexture)
+    [CreateAssetMenu]
+    [System.Serializable]
+    public class InferenceFeatureVision : InferenceFeature
     {
-
-    }
-
-
-    public virtual void Inference(byte[] inputData)
-    {
-
-    }
+        public InputTexture inputTexture;
 
 
-    /// <summary>
-    /// Calculate the dimensions for the input image
-    /// </summary>
-    /// <param name="newVideo"></param>
-    public void InitializeTextures()
-    {
+        [Tooltip("The target resolution for input images")]
+        public Vector2Int targetDims = new Vector2Int(640, 640);
 
-        // Adjust the input dimensions to maintain the current aspect ratio
-        if (imageDims.x != targetDims.x)
+
+        // The unpadded dimensions of the image being fed to the model
+        public Vector2Int imageDims = new Vector2Int(0, 0);
+
+        // Used to scale the input image dimensions while maintaining aspect ratio
+        protected float aspectRatioScale;
+
+
+        public virtual void Inference(RenderTexture renderTexture)
         {
-            imageDims.x = targetDims.x;
-            aspectRatioScale = (float)inputTexture.renderTexture.height / inputTexture.renderTexture.width;
-            imageDims.y = (int)(targetDims.x * aspectRatioScale);
-            targetDims.y = imageDims.y;
-
-        }
-        if (imageDims.y != targetDims.y)
-        {
-            imageDims.y = targetDims.y;
-            aspectRatioScale = (float)inputTexture.renderTexture.width / inputTexture.renderTexture.height;
-            imageDims.x = (int)(targetDims.y * aspectRatioScale);
-            targetDims.x = imageDims.x;
 
         }
 
-        // Update the values for the width and height input fields
-        Debug.Log($"Setting Input Dims for {this.name} to W: {imageDims.x} x H: {imageDims.y}");
-    }
 
+        public virtual void Inference(byte[] inputData)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Calculate the dimensions for the input image
+        /// </summary>
+        /// <param name="newVideo"></param>
+        public void InitializeTextures()
+        {
+
+            // Adjust the input dimensions to maintain the current aspect ratio
+            if (imageDims.x != targetDims.x)
+            {
+                imageDims.x = targetDims.x;
+                aspectRatioScale = (float)inputTexture.renderTexture.height / inputTexture.renderTexture.width;
+                imageDims.y = (int)(targetDims.x * aspectRatioScale);
+                targetDims.y = imageDims.y;
+
+            }
+            if (imageDims.y != targetDims.y)
+            {
+                imageDims.y = targetDims.y;
+                aspectRatioScale = (float)inputTexture.renderTexture.width / inputTexture.renderTexture.height;
+                imageDims.x = (int)(targetDims.y * aspectRatioScale);
+                targetDims.x = imageDims.x;
+
+            }
+
+            // Update the values for the width and height input fields
+            Debug.Log($"Setting Input Dims for {this.name} to W: {imageDims.x} x H: {imageDims.y}");
+        }
+
+    }
 }
+
+
