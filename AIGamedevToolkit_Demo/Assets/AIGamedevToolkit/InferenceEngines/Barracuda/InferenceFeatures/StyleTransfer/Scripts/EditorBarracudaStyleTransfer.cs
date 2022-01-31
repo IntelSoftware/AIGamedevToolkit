f9d99e55e21bf8c917aa113e83b62da03d4f3c5b
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-31 13:48:12 -0800

    Update EditorBarracudaStyleTransfer.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/Barracuda/InferenceFeatures/StyleTransfer/Scripts/EditorBarracudaStyleTransfer.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/Barracuda/InferenceFeatures/StyleTransfer/Scripts/EditorBarracudaStyleTransfer.cs
@@ -1,19 +1,28 @@
 namespace AIGamedevToolkit
 {
-#if UNITY_EDITOR
+    // Only draw custom editor when in the Unity Editor
+    #if UNITY_EDITOR
     using UnityEditor;
 
+    /// <summary>
+    /// A custom editor for InferenceFeatureBarracudaStyleTransfer assets
+    /// </summary>
     [CustomEditor(typeof(InferenceFeatureBarracudaStyleTransfer))]
     public class EditorBarracudaStyleTransfer : Editor
     {
+        /// <summary>
+        /// Draw a custom Inspector GUI for the inference feature
+        /// </summary>
         public override void OnInspectorGUI()
         {
+            // Get a reference to the associated inference feature
             InferenceFeatureBarracudaStyleTransfer scriptableInferenceFeature = (InferenceFeatureBarracudaStyleTransfer)target;
+            // Draw the editor user interface for the inference feature
             scriptableInferenceFeature.DrawUI();
         }
 
     }
-#endif
+    #endif
 }
 
 
