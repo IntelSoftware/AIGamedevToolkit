e7acda67fe6bd0e635b506c9d64f5e653dba9aaf
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-31 15:51:27 -0800

    Update EditorOpenVINOStyleTransfer.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/OpenVINO/InferenceFeatures/StyleTransfer/Scripts/EditorOpenVINOStyleTransfer.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/OpenVINO/InferenceFeatures/StyleTransfer/Scripts/EditorOpenVINOStyleTransfer.cs
@@ -1,26 +1,27 @@
-using UnityEngine;
-
-
 namespace AIGamedevToolkit
 {
-#if UNITY_EDITOR
+    // Only draw custom editor when in the Unity Editor
+    #if UNITY_EDITOR
     using UnityEditor;
 
+    /// <summary>
+    /// A custom editor for InferenceFeatureOpenVINOStyleTransfer assets
+    /// </summary>
     [CustomEditor(typeof(InferenceFeatureOpenVINOStyleTransfer))]
     public class EditorOpenVINOStyleTransfer : Editor
     {
         /// <summary>
-        /// 
+        /// Draw a custom Inspector GUI for the inference feature
         /// </summary>
         public override void OnInspectorGUI()
         {
+            // Get a reference to the associated inference feature
             InferenceFeatureOpenVINOStyleTransfer scriptableInferenceFeature = (InferenceFeatureOpenVINOStyleTransfer)target;
-
+            // Draw the editor user interface for the inference feature
             scriptableInferenceFeature.DrawUI();
         }
     }
-
-#endif
+    #endif
 }
 
 
