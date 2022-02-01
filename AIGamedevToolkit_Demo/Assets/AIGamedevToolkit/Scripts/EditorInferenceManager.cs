7d6f2f1050a03df540d3217db4085b43d72b4050
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-31 23:39:41 -0800

    Update EditorInferenceManager.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/EditorInferenceManager.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/EditorInferenceManager.cs
@@ -1,13 +1,15 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 namespace AIGamedevToolkit
 {
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
     using UnityEditor;
 
+    /// <summary>
+    /// A custom editor for InferenceManager components
+    /// </summary>
     [CustomEditor(typeof(InferenceManager))]
     public class EditorInferenceManager : Editor
     {
@@ -15,15 +17,31 @@ namespace AIGamedevToolkit
         private bool unfold = false;
         private string modelInferenceFeatureSettingsLabel = "Inference Feature Settings";
 
+        private List<InferenceFeature> inferenceFeatures;
 
+        /// <summary>
+        /// Draw a custom Inspector GUI for the inference manager
+        /// </summary>
         public override void OnInspectorGUI()
         {
+            // Get a reference to the associated inference manager component
             InferenceManager inferenceManager = (InferenceManager)target;
 
-            List<InferenceFeature> inferenceFeatures = inferenceManager.inferenceFeatureList;
+            if (inferenceFeatures == null)
+            {
+                inferenceFeatures = inferenceManager.inferenceFeatureList;
+            }
+            else if (inferenceFeatures != inferenceManager.inferenceFeatureList)
+            {
+                // Get the list of inference features attached to the inference manager component
+                inferenceFeatures = inferenceManager.inferenceFeatureList;
+                // Save changes to properties
+                EditorUtility.SetDirty(inferenceManager);
+            }
 
+            // Draw the default editor user interface for the inference manager
             base.OnInspectorGUI();
-
+            // Draw the custom editors for each attached inference feature in a foldout submenu
             unfold = inferenceManager.showInferenceFeatureSettings;
             unfold = EditorGUILayout.Foldout(unfold, modelInferenceFeatureSettingsLabel);
             if (unfold)
@@ -39,9 +57,8 @@ namespace AIGamedevToolkit
             }
             inferenceManager.showInferenceFeatureSettings = unfold;
         }
-
     }
-#endif
+    #endif
 }
 
 
