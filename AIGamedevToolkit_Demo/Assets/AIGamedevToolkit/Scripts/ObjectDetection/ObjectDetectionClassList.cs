9980d7503af0972ddbfac14f242642334092aff6
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-02-01 00:23:18 -0800

    Update ObjectDetectionClassList.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/ObjectDetection/ObjectDetectionClassList.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/ObjectDetection/ObjectDetectionClassList.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace AIGamedevToolkit
 {
+    /// <summary>
+    /// A base for defining lists of object classes for object detection models
+    /// </summary>
     [System.Serializable]
     public class ObjectDetectionClassList : ScriptableObject
     {
