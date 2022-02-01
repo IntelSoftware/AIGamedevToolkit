508f0f5db7eba5839e65571ec4361d2819339598
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-31 18:34:13 -0800

    Update EditorOpenVINOYOLOX.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/OpenVINO/InferenceFeatures/YOLOX/Scripts/EditorOpenVINOYOLOX.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/InferenceEngines/OpenVINO/InferenceFeatures/YOLOX/Scripts/EditorOpenVINOYOLOX.cs
@@ -1,23 +1,27 @@
-using UnityEngine;
-
-
 namespace AIGamedevToolkit
 {
-#if UNITY_EDITOR
+    // Only draw custom editor when in the Unity Editor
+    #if UNITY_EDITOR
     using UnityEditor;
 
+    /// <summary>
+    /// A custom editor for InferenceFeatureOpenVINOYOLOX assets
+    /// </summary>
     [CustomEditor(typeof(InferenceFeatureOpenVINOYOLOX))]
     public class EditorOpenVINOYOLOX : Editor
     {
+        /// <summary>
+        /// Draw a custom Inspector GUI for the inference feature
+        /// </summary>
         public override void OnInspectorGUI()
         {
+            // Get a reference to the associated inference feature
             InferenceFeatureOpenVINOYOLOX scriptableInferenceFeature = (InferenceFeatureOpenVINOYOLOX)target;
-
+            // Draw the editor user interface for the inference feature
             scriptableInferenceFeature.DrawUI();
         }
     }
-
-#endif
+    #endif
 }
 
 
