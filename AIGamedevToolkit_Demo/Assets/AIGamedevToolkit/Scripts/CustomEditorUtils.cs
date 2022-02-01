daa2f7318ea2ba50abafc1c9717c1811d0d76d89
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-01-31 23:39:35 -0800

    Update CustomEditorUtils.cs

--- AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/CustomEditorUtils.cs
+++ AIGamedevToolkit_Demo/Assets/AIGamedevToolkit/Scripts/CustomEditorUtils.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 namespace AIGamedevToolkit
 {
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
     using UnityEditor;
     public class CustomEditorUtils
     {
         /// <summary>
-        /// 
+        /// Copies the OpenVINO model files for a ModelOpenVINO asset to the StreamingAssets folder
         /// </summary>
         /// <param name="model"></param>
         /// <param name="streamingAssetsDir"></param>
         public static void CopyToStreamingAssets(ModelOpenVINO model, string streamingAssetsDir)
         {
+            // Create a StreamingAssets folder if one does not already exist
             if (AssetDatabase.IsValidFolder(streamingAssetsDir) == false)
             {
                 Debug.Log("Creating StreamingAssets folder.");
@@ -21,23 +22,23 @@ namespace AIGamedevToolkit
 
             string exportPath = streamingAssetsDir + "/" + model.streamingAssetsPath;
 
+            // Create the specified directory in the StreamingAssets folder if it does not exist
             if (AssetDatabase.IsValidFolder(exportPath) == false)
             {
                 AssetDatabase.CreateFolder(streamingAssetsDir, model.streamingAssetsPath);
             }
 
-            Debug.Log("Copying models folder to StreamingAssets folder.");
             string modelPath = model.modelPath;
             string modelDir = modelPath.Substring(0, modelPath.LastIndexOf("/") + 1);
-
             string fileName = modelPath.Substring(0, modelPath.LastIndexOf("."));
             fileName = fileName.Substring(modelPath.LastIndexOf("/") + 1);
 
+            // Copy model files to StreamingAssets folder
             string xmlFileName = fileName + ".xml";
             AssetDatabase.CopyAsset(modelDir + xmlFileName, exportPath + "/" + xmlFileName);
             string binFileName = fileName + ".bin";
             AssetDatabase.CopyAsset(modelDir + binFileName, exportPath + "/" + binFileName);           
         }
     }
-#endif
+    #endif
 }
