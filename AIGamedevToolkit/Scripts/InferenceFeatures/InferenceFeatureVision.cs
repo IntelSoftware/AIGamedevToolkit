fcfdb37d0611db924c5f755c5dc6d672a1e2e067
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-02-11 11:27:54 -0800

    Update InferenceFeatureVision.cs

--- AIGamedevToolkit/Scripts/InferenceFeatures/InferenceFeatureVision.cs
+++ AIGamedevToolkit/Scripts/InferenceFeatures/InferenceFeatureVision.cs
@@ -34,7 +34,7 @@ namespace AIGamedevToolkit
         public override void ApplyToScene()
         {
             base.ApplyToScene();
-            Utils.AddCameraTextureHelper();
+            Utils.AddTextureHelper();
         }
 
 
